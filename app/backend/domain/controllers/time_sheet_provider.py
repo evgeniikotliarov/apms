d73df0434992ebc90a56891890c9f611a5b04771
evgenii <kotlyarov.ev@gmail.com>
2018-11-30 14:32:16 +0300

    Добавил модель Time Sheet с провайдером, так же добавил exception на попытку обновить или закрыть таблицу

---
+++ app/backend/domain/controllers/time_sheet_provider.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from domain.exceptions import AccessDeniedToUpdateTimeSheet
+from domain.models.time_sheet import TimeSheet
+
+
+class TimeSheetProvider:
+    @staticmethod
+    def create(time_sheet_id, date: datetime, employee_id, employee_rate, norm, work_days_sheet):
+        time_sheet = TimeSheet()
+        time_sheet.id = time_sheet_id
+        time_sheet.norm = norm
+        time_sheet.employee_rate = employee_rate
+        time_sheet.year = date.year
+        time_sheet.month = date.month
+        time_sheet.sheet = work_days_sheet
+        time_sheet.employee_id = employee_id
+        time_sheet.closed = False
+        return time_sheet
+
+    @staticmethod
+    def update_with(time_sheet: TimeSheet, norm=None, work_days_sheet=None):
+        if time_sheet.closed:
+            raise AccessDeniedToUpdateTimeSheet()
+        time_sheet.norm = norm if norm else time_sheet.norm
+        time_sheet.sheet = work_days_sheet if work_days_sheet else time_sheet.sheet
+        return time_sheet
+
+    @staticmethod
+    def close(time_sheet: TimeSheet):
+        time_sheet.closed = True
+        return time_sheet
---
+++ app/backend/domain/models/time_sheet.py
@@ -0,0 +1,10 @@
+class TimeSheet:
+    def __init__(self):
+        self.id = None
+        self.norm = None
+        self.rate = None
+        self.year = None
+        self.month = None
+        self.sheet = None
+        self.employee_id = None
+        self.closed = None
