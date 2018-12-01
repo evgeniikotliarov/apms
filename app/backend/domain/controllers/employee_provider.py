5ca31b8fb765a6be1f8432a3de99852b8b785044
evgenii <kotlyarov.ev@gmail.com>
2018-12-01 21:08:42 +0300

    Переделал staticmethod в classmethod

--- app/backend/domain/controllers/employee_provider.py
+++ app/backend/domain/controllers/employee_provider.py
@@ -4,8 +4,8 @@ from domain.models.employee import Employee
 
 
 class EmployeeProvider:
-    @staticmethod
-    def create_simple(name, password, email):
+    @classmethod
+    def create_simple(cls, name, password, email):
         employee = Employee()
         employee.name = name
         employee.password = password
@@ -13,33 +13,33 @@ class EmployeeProvider:
         employee.activated = False
         return employee
 
-    @staticmethod
-    def register(employee: Employee, employment_date, balance_vac=0):
+    @classmethod
+    def register(cls, employee: Employee, employment_date, balance_vac=0):
         employee.registration_date = datetime.now()
         employee.employment_date = employment_date
         employee.vacation = balance_vac
         employee.activated = True
         return employee
 
-    @staticmethod
-    def update_with(employee, name=None, password=None, email=None, employment_date=None):
+    @classmethod
+    def update_with(cls, employee, name=None, password=None, email=None, employment_date=None):
         employee.employment_date = employment_date if employment_date else employee.employment_date
         employee.password = password if password else employee.password
         employee.email = email if email else employee.email
         employee.name = name if name else employee.name
         return employee
 
-    @staticmethod
-    def activate(employee: Employee):
+    @classmethod
+    def activate(cls, employee: Employee):
         employee.activated = True
         return employee
 
-    @staticmethod
-    def deactivate(employee: Employee):
+    @classmethod
+    def deactivate(cls, employee: Employee):
         employee.activated = False
         return employee
 
-    @staticmethod
-    def set_balance_vac(employee: Employee, balance_vac):
+    @classmethod
+    def set_balance_vac(cls, employee: Employee, balance_vac):
         employee.vacation = balance_vac
         return employee
