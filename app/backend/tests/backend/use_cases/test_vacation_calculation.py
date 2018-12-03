28f9b21d97c52ec80bb3ab08456171e626973608
evgenii <kotlyarov.ev@gmail.com>
2018-12-03 14:03:00 +0300

    Добавил калькуляцию отпускных дней

--- app/backend/tests/backend/use_cases/test_vacation_calculation.py
+++ app/backend/tests/backend/use_cases/test_vacation_calculation.py
@@ -1,19 +1,56 @@
 import unittest
+from datetime import datetime
 
 from domain.controllers.employee_provider import EmployeeProvider
-from storages.storages import EmployeesStorage
+from domain.controllers.rate_calculator import RateCalculator
+from domain.controllers.time_sheet_provider import TimeSheetProvider
+from domain.controllers.vacation_calculator import VacationCalculator
+from storages.storages import EmployeesStorage, TimeSheetsStorage
 # noinspection PyPackageRequirements
-from tests.fake_db import FakeEmployeesDb
+from tests.fake_db import FakeEmployeesDb, FakeTimeSheetsDb
+from tests.fixtures.sheets import january, half_january
+from usecases.calculate_vacation_use_case import CalculateVacationUseCase
 
 
 class TestProvideEmployeeUseCase(unittest.TestCase):
-    def test_update_employees_vacation(self):
+    def test_calculate_vacation(self):
         employee_storage = EmployeesStorage(FakeEmployeesDb())
-        time_sheet_storage = TimeSheetStorage(FakeTimeSheetDb())
+        time_sheet_storage = TimeSheetsStorage(FakeTimeSheetsDb())
         employee_controller = EmployeeProvider()
-        time_sheet_controller = EmployeeProvider()
-        use_case = CalculateVacationUseCase(employee_controller, time_sheet_controller,
+        time_sheet_controller = TimeSheetProvider()
+        calculator = VacationCalculator()
+        use_case = CalculateVacationUseCase(employee_controller, time_sheet_controller, calculator,
                                             employee_storage, time_sheet_storage)
-
+        employee = employee_storage.find_by_id(0)
+        employee.vacation = 10
+        employee.employment_date = datetime(2016, 8, 1)
+        use_case.calculate_vacation(0, january, datetime(2018, 1, 1))
         saved_employee = employee_storage.find_by_id(0)
-        self.assertEqual(saved_employee.is_admin(), False)
+        self.assertEqual(saved_employee.vacation, 13.29)
+
+        employee.vacation = 10
+        employee.employment_date = datetime(2017, 12, 1)
+        use_case.calculate_vacation(0, january, datetime(2018, 1, 1))
+        saved_employee = employee_storage.find_by_id(0)
+        self.assertEqual(saved_employee.vacation, 12.3)
+
+    def test_calculate_vacation_with_minus(self):
+        employee_storage = EmployeesStorage(FakeEmployeesDb())
+        time_sheet_storage = TimeSheetsStorage(FakeTimeSheetsDb())
+        employee_controller = EmployeeProvider()
+        time_sheet_controller = TimeSheetProvider()
+        calculator = VacationCalculator()
+        use_case = CalculateVacationUseCase(employee_controller, time_sheet_controller, calculator,
+                                            employee_storage, time_sheet_storage)
+        employee = employee_storage.find_by_id(0)
+        employee.vacation = 10
+        employee.employment_date = datetime(2016, 8, 1)
+        use_case.calculate_vacation(0, half_january, datetime(2018, 1, 1))
+        saved_employee = employee_storage.find_by_id(0)
+        self.assertEqual(saved_employee.vacation, 4.14)
+
+        employee.vacation = 10
+        employee.employment_date = datetime(2017, 12, 1)
+        use_case.calculate_vacation(0, half_january, datetime(2018, 1, 1))
+        saved_employee = employee_storage.find_by_id(0)
+        self.assertEqual(saved_employee.vacation, 3.5)
--- app/backend/usecases/calculate_vacation_use_case.py
+++ app/backend/usecases/calculate_vacation_use_case.py
@@ -1,18 +1,32 @@
+from datetime import datetime
+
 from domain.controllers.employee_provider import EmployeeProvider
 from domain.controllers.time_sheet_provider import TimeSheetProvider
-from storages.storages import EmployeesStorage
+from domain.controllers.vacation_calculator import VacationCalculator
+from storages.storages import EmployeesStorage, TimeSheetsStorage
 
 
 class CalculateVacationUseCase:
-    def __init__(self, employee_provider: EmployeeProvider, time_sheet_provider: TimeSheetProvider,
-                 time_sheet_storage: TimeSheetStorage, employee_storage: EmployeesStorage):
+    def __init__(self, employee_provider: EmployeeProvider,
+                 time_sheet_provider: TimeSheetProvider,
+                 calculator: VacationCalculator,
+                 employee_storage: EmployeesStorage,
+                 time_sheet_storage: TimeSheetsStorage):
         self.employee_provider = employee_provider
         self.time_sheet_provider = time_sheet_provider
+        self.calculator = calculator
         self.employee_storage = employee_storage
         self.time_sheet_storage = time_sheet_storage
 
-    def calculate_vacation(self, employee_id):
+    def calculate_vacation(self, employee_id, sheet: dict,
+                           date: datetime = datetime.now(), norm=None):
         employee = self.employee_storage.find_by_id(employee_id)
-        time_sheet = self.time_sheet_provider
-        self.storage.update(employee)
-        return employee
+        time_sheet = self.time_sheet_provider.create(
+            date=date, work_days_sheet=sheet, employee_id=employee.id,
+            employee_rate=employee.rate)
+        if not norm:
+            time_sheet = self.calculator.calculate_norm(time_sheet)
+        time_sheet = self.calculator.calculate_vacation(time_sheet)
+        employee.vacation += time_sheet.vacation
+        self.employee_storage.update(employee)
+        self.time_sheet_storage.save(time_sheet)
