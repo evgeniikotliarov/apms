2dc96cf46d33b9112f1e54072350fe911d293c8f
evgenii <kotlyarov.ev@gmail.com>
2018-12-06 23:03:21 +0300

    Добавил use_case k App

--- app/backend/app.py
+++ app/backend/app.py
@@ -4,3 +4,17 @@ import falcon
 class App(falcon.API):
     def __init__(self):
         super().__init__()
+        self.get_employee_use_case = None
+        self.get_employees_use_case = None
+        self.create_employee_use_case = None
+        self.register_employee_use_case = None
+        self.update_employee_use_case = None
+        self.admin_rights_employee_use_case = None
+
+        self.get_time_sheet_use_case = None
+        self.get_time_sheets_use_case = None
+        self.create_time_sheet_use_case = None
+        self.update_time_sheet_use_case = None
+        self.close_time_sheet_use_case = None
+
+        self.calculate_vacation_use_case = None
--- app/backend/app_factory.py
+++ app/backend/app_factory.py
@@ -1,11 +1,52 @@
 from app import App
+from domain.controllers.employee_provider import EmployeeProvider
+from domain.controllers.time_sheet_provider import TimeSheetProvider
+from storages.db.db_builder import DbBuilder
+from storages.storages import TimeSheetsStorage, EmployeesStorage
+from usecases.employee_use_cases import CreateEmployeeUseCase, GetEmployeeUseCase, \
+    GetAllEmployeeUseCase, RegisterEmployeeUseCase, UpdateEmployeeUseCase, \
+    AdminRightsEmployeeUseCase
+from usecases.time_sheet_use_cases import GetTimeSheetUseCase, GetAllTimeSheetUseCase, \
+    CreateTimeSheetUseCase, UpdateTimeSheetUseCase, CloseTimeSheetUseCase
 
 
 class IAppFactory:
-    def create_app(self):
+    def create_app(self) -> App:
         raise NotImplementedError()
 
 
+# noinspection PyAttributeOutsideInit
 class AppFactory(IAppFactory):
     def create_app(self):
-        return App()
+        app = App()
+        db = DbBuilder().build()
+
+        employee_storage = EmployeesStorage(db)
+        employee_provider = EmployeeProvider()
+
+        app.get_employee_use_case = GetEmployeeUseCase(employee_provider,
+                                                       employee_storage)
+        app.get_employees_use_case = GetAllEmployeeUseCase(employee_provider,
+                                                           employee_storage)
+        app.create_employee_use_case = CreateEmployeeUseCase(employee_provider,
+                                                             employee_storage)
+        app.register_employee_use_case = RegisterEmployeeUseCase(employee_provider,
+                                                                 employee_storage)
+        app.update_employee_use_case = UpdateEmployeeUseCase(employee_provider,
+                                                             employee_storage)
+        app.admin_rights_employee_use_case = AdminRightsEmployeeUseCase(employee_provider,
+                                                                        employee_storage)
+
+        time_sheet_storage = TimeSheetsStorage(db)
+        time_sheet_provider = TimeSheetProvider()
+
+        app.get_time_sheet_use_case = GetTimeSheetUseCase(time_sheet_provider,
+                                                          time_sheet_storage)
+        app.get_time_sheets_use_case = GetAllTimeSheetUseCase(time_sheet_provider,
+                                                              time_sheet_storage)
+        app.create_time_sheet_use_case = CreateTimeSheetUseCase(time_sheet_provider,
+                                                                time_sheet_storage)
+        app.update_time_sheet_use_case = UpdateTimeSheetUseCase(time_sheet_provider,
+                                                                time_sheet_storage)
+        app.close_time_sheet_use_case = CloseTimeSheetUseCase(time_sheet_provider,
+                                                              time_sheet_storage)
