8c04fce53c0f66326505cfd146a179ed710da9e1
evgenii <kotlyarov.ev@gmail.com>
2018-12-09 13:03:49 +0300

    Переделал серриализацию

--- app/backend/domain/controllers/employee_provider.py
+++ app/backend/domain/controllers/employee_provider.py
@@ -8,8 +8,8 @@ class EmployeeProvider:
     def create_simple(cls, name, password, email):
         employee = Employee()
         employee.name = name
-        employee.password = password
         employee.email = email
+        employee.password = password
         employee.activated = False
         employee.is_admin = False
         return employee
@@ -18,13 +18,13 @@ class EmployeeProvider:
     def register(cls, employee: Employee, employment_date: datetime, balance_vac=0):
         employee.registration_date = datetime.now()
         employee.employment_date = employment_date
-        employee.vacation = balance_vac
         employee.activated = True
+        employee.vacation = balance_vac
         return employee
 
     @classmethod
-    def update_with(cls, employee, name=None, password=None, email=None,
-                    employment_date: datetime=None):
+    def update_with(cls, employee: Employee, name=None, password=None, email=None,
+                    employment_date: datetime = None):
         employee.employment_date = employment_date if employment_date else employee.employment_date
         employee.password = password if password else employee.password
         employee.email = email if email else employee.email
@@ -58,17 +58,30 @@ class EmployeeProvider:
 
     @classmethod
     def serialize(cls, employee: Employee):
+        serialized_employee = {
+            'id': employee.id,
+            'name': employee.name,
+            'password': employee.password,
+            'email': employee.email,
+            'vacation': employee.vacation,
+            'is_admin': employee.is_admin,
+            'activated': employee.activated,
+            'employment_date': None,
+            'registration_date': None
+        }
         if employee.employment_date:
-            employee.employment_date = employee.employment_date.strftime('Y%.%m.%d')
+            serialized_employee['employment_date'] = employee.employment_date \
+                .strftime('%Y.%m.%d')
         if employee.registration_date:
-            employee.registration_date = employee.registration_date.strftime('Y%.%m.%d')
-        return employee.__dict__
+            serialized_employee['registration_date'] = employee.registration_date \
+                .strftime('%Y.%m.%d')
+        return serialized_employee
 
     @classmethod
     def deserialize(cls, serialized_employee: dict):
         employee = Employee()
         for key, value in serialized_employee.items():
             if 'date' in key and value:
-                value = datetime.strftime(value, 'Y%.%m.%d')
+                value = datetime.strptime(value, '%Y.%m.%d')
             setattr(employee, key, value)
         return employee
