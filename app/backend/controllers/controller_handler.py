ea4710ed71cf5707d0f4b0fb5f98dd5057ab9051
evgenii <kotlyarov.ev@gmail.com>
2018-12-10 15:42:58 +0300

    Обновил модуль, добавил проверку

--- app/backend/controllers/controller_handler.py
+++ app/backend/controllers/controller_handler.py
@@ -1,17 +1,38 @@
 import falcon
 
-from exceptions import BaseError
+from exceptions import BaseError, InvalidTokenError
+from utils.tokenizer import Tokenizer
+
+CONTROLLER = 0
+REQUEST = 1
+RESPONSE = 2
+
+tokenizer = Tokenizer()
 
 
 def controller_handler(controller):
     def wrap(*args, **kwargs):
         try:
-            # TODO: not auth
-            # if not auth(request):
-            #     raise BadRequestError
-            args[2].status = falcon.HTTP_200
+            args[RESPONSE].status = falcon.HTTP_200
             controller(*args, **kwargs)
         except BaseError as error:
-            args[2].status = error.get_http_code()
+            args[RESPONSE].status = error.get_http_code()
+
+    return wrap
+
+
+def authorized_controller_handler(controller):
+    def wrap(*args, **kwargs):
+        try:
+            raw_token = args[REQUEST].headers['Authorization']
+            if not raw_token:
+                raise InvalidTokenError()
+            data = tokenizer.get_data_by_token(raw_token)
+            user_id, user_email = data['id'], data['email']
+            args[CONTROLLER].user_email = user_email
+            args[RESPONSE].status = falcon.HTTP_200
+            controller(*args, **kwargs)
+        except BaseError as error:
+            args[RESPONSE].status = error.get_http_code()
 
     return wrap
