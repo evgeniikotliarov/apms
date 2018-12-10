46fa6c6ca4873fc19baad14a59def973bb78749f
evgenii <kotlyarov.ev@gmail.com>
2018-12-10 17:30:20 +0300

    Добавил/исправил тесты на получение пользователей,
    отрефакторил подгрузку пользователей

--- app/backend/tests/fake_app_factory.py
+++ app/backend/tests/fake_app_factory.py
@@ -13,20 +13,18 @@ class TestAppFactory(IAppFactory):
 
 
 class TestCopyOriginalAppFactory(AppFactory):
+    def __init__(self):
+        super().__init__()
+        self.tokens = {}
+
     def create_app(self):
         app = super().create_app()
         self.load_data(app)
         return app
 
     def load_data(self, app):
-        serialized_employee = fixtures.load("admin_user")
-        serialized_employee['password'] = ToHash().to_hash(serialized_employee['password'])
-        admin_employee = self.employee_provider.deserialize(serialized_employee)
-        serialized_employee = fixtures.load("unregistered_user")
-        serialized_employee['password'] = ToHash().to_hash(serialized_employee['password'])
-        unregistered_employee = self.employee_provider.deserialize(serialized_employee)
-        self.employee_storage.save(admin_employee)
-        self.employee_storage.save(unregistered_employee)
+        self.__load_user('admin_user')
+        self.__load_user('unregistered_user')
         app.create_time_sheet_use_case.create_time_sheet(
             date=datetime(2018, 1, 1),
             sheet=fixtures.load("january"),
@@ -34,3 +32,13 @@ class TestCopyOriginalAppFactory(AppFactory):
             rate=RateCalculator.MAX_DAYS,
             norm=23
         )
+
+    def __load_user(self, name):
+        serialized_employee = fixtures.load(name)
+        password = ToHash().to_hash(serialized_employee['password'])
+        serialized_employee['password'] = password
+        employee = self.employee_provider.deserialize(serialized_employee)
+        self.employee_storage.save(employee)
+        email = serialized_employee['email']
+        data = {'email': email, 'password': password}
+        self.tokens[email] = self.tokenizer.get_token_by_data(data)
--- app/backend/tests/test_controllers/test_employees_controllers.py
+++ app/backend/tests/test_controllers/test_employees_controllers.py
@@ -3,6 +3,7 @@ import unittest
 
 from falcon import testing, falcon
 
+from tests import fixtures
 from tests.fake_app_factory import TestCopyOriginalAppFactory
 
 
@@ -31,3 +32,25 @@ class TestEmployeesControllers(unittest.TestCase):
 
         response = self.client.simulate_post('/api/sign-in', body=json.dumps(data))
         self.assertEqual(response.status, falcon.HTTP_200)
+        self.assertIsNotNone(response.json['token'])
+
+    def test_get_employee(self):
+        employee = fixtures.load('admin_user')
+        headers = self.__get_authorization_header_for(employee)
+        response = self.client.simulate_get('/api/employees/0', headers=headers)
+        del employee['password']
+        self.assertEqual(response.json, employee)
+
+    def test_get_employees(self):
+        employee = fixtures.load('admin_user')
+        headers = self.__get_authorization_header_for(employee)
+        response = self.client.simulate_get('/api/employees/', headers=headers)
+        self.assertTrue(response.json.__len__() > 0)
+        first_employee = response.json[0]
+        del employee['password']
+        self.assertEqual(first_employee, employee)
+
+    def __get_authorization_header_for(self, employee):
+        email = employee['email']
+        token = self.factory.tokens[email]
+        return {'Authorization': token}
