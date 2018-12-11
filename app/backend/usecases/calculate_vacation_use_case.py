0415f81464be35b0e118d05808091c502fcfbd08
evgenii <kotlyarov.ev@gmail.com>
2018-12-11 15:11:56 +0300

    Отоефакторил Калькуляцию дней

--- app/backend/usecases/calculate_vacation_use_case.py
+++ app/backend/usecases/calculate_vacation_use_case.py
@@ -4,26 +4,26 @@ from storages.storages import EmployeesStorage, TimeSheetsStorage
 
 class CalculateVacationUseCase:
     def __init__(self,
-                 calculator: VacationCalculator,
+                 vac_calculator: VacationCalculator,
                  employee_storage: EmployeesStorage,
                  time_sheet_storage: TimeSheetsStorage):
-        self.calculator = calculator
+        self.vac_calculator = vac_calculator
         self.employee_storage = employee_storage
         self.time_sheet_storage = time_sheet_storage
 
-    def calculate_vacation(self, time_sheet_id):
+    def calculate_vacation(self, time_sheet_id, norm=None):
         time_sheet = self.time_sheet_storage.find_by_id(time_sheet_id)
         employee = self.employee_storage.find_by_id(time_sheet.employee_id)
         time_sheet.rate = employee.rate
+        if norm:
+            time_sheet.norm = norm
         if time_sheet.vacation:
             employee.vacation -= time_sheet.vacation
         time_sheet = self._calculate_vac(time_sheet)
         self._update_employee_vacation(employee, time_sheet)
 
     def _calculate_vac(self, time_sheet):
-        if not time_sheet.norm:
-            time_sheet = self.calculator.calculate_norm(time_sheet)
-        time_sheet = self.calculator.calculate_vacation(time_sheet)
+        time_sheet = self.vac_calculator.calculate_vacation(time_sheet)
         return time_sheet
 
     def _update_employee_vacation(self, employee, time_sheet):
