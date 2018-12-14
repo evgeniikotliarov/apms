60586b0ccc8b49630529d84388d855f3b94b5a2a
evgenii <kotlyarov.ev@gmail.com>
2018-12-14 15:57:27 +0300

    Переделал базовый App

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from "./components/login/Login";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      navbar: false,
+      loading: false,
+      redirect: false
+
+    };
   }
+
+  renderNavbar = (props) => this.state.navbar ? <NavbarEmployee {...props} /> : null;
+
+  render = () => {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/login" component={(props) => <Login {...props}/>}/>
+        </div>
+      </Router>
+    );
+  };
+
 }
 
 export default App;
