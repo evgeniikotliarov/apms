863733207657518c921ce4e3aded2c7f31058dcf
evgenii <kotlyarov.ev@gmail.com>
2018-12-15 23:24:20 +0300

    Добавил компонент NotFound, для несуществующей страницы

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -8,14 +8,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      navbar: false,
       loading: false,
       redirect: false
 
     };
   }
-
-  renderNavbar = (props) => this.state.navbar ? <NavbarEmployee {...props} /> : null;
 
   render = () => {
     return (
