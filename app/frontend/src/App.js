7fa3917a434b8d6df6acda0bf88e3c05e64a568e
evgenii <kotlyarov.ev@gmail.com>
2018-12-16 17:22:54 +0300

    Сделал страницу Регистрации

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from "./components/login/Login";
-import Signup from "./components/signup/Signup";
-import NavbarEmployee from "./components/navbar/Navbar";
-import NotFound from "./components/not_found/NotFound";
+import Signup from "./components/signup/SignUp";
+// import NavbarEmployee from "./components/navbar/Navbar";
+// import NotFound from "./components/not_found/NotFound";
 
 class App extends Component {
 
@@ -23,8 +23,8 @@ class App extends Component {
         <div>
           <Route exact path="/login" component={(props) => <Login {...props}/>}/>
           <Route exact path="/signup" component={(props) => <Signup {...props}/>}/>
-          <Route exact path="/" component={(props) => this.renderNavbar(props)}/>
-          <Route exact path="*" component={(props) => <NotFound {...props}/>}/>
+          {/*<Route exact path="/" component={(props) => this.renderNavbar(props)}/>*/}
+          {/*<Route exact path="*" component={(props) => <NotFound {...props}/>}/>*/}
         </div>
       </Router>
     );
---
+++ app/frontend/src/components/signup/SignUp.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import './SignUp.css';
+
+
+class Signup extends Component {
+
+  state = {
+    email: '',
+    fullname: '',
+    password: '',
+    errorMessage: null,
+    loading: true
+  };
+
+  emailChange = (e) => {
+    this.setState({email: e.target.value});
+  };
+
+  fullnameChange = (e) => {
+    this.setState({fullname: e.target.value});
+  };
+
+  passwordChange = (e) => {
+    this.setState({password: e.target.value});
+  };
+
+  onLoginClick = () => {
+    this.props.history.push('/login')
+  };
+
+  render = () => {
+    const message = this.state.errorMessage ? <div className="message">{ this.state.errorMessage }</div> : null;
+    return (
+      <div className="Signup">
+        <h1 className="h1">Регистрация</h1>
+        { message }
+        <form>
+          <label className="for-label">Email:</label>
+          <input className="for-input" type="text" placeholder="Email"
+                 onChange={this.emailChange}
+                 value={this.state.email}/>
+          <label className="for-label">Имя:</label>
+          <input className="for-input" type="text" placeholder="Name"
+                 onChange={this.fullnameChange}
+                 value={this.state.fullname}/>
+          <label className="for-label">Пароль:</label>
+          <input className="for-input" type="password" placeholder="Password"
+                 onChange={this.passwordChange}
+                 value={this.state.password}/>
+          <button className="btn-login"
+                  onClick={this.onSignupClick}
+                  type="submit">Зарегистрироваться</button>
+        </form>
+        <span>Уже зарегистрированы?</span>
+        <span onClick={this.onLoginClick} className="for-signup">Войти</span>
+      </div>
+    )
+  }
+}
+
+export default withRouter(Signup);
