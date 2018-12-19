3ec117c3e9ccb3f8b84ccf2219ce0fff60fef979
evgenii <kotlyarov.ev@gmail.com>
2018-12-19 10:40:49 +0300

    Добавил вытягивание профиля (с апи) на страницу профиля

--- app/frontend/src/components/profile/Profile.js
+++ app/frontend/src/components/profile/Profile.js
@@ -1,24 +1,31 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import './Profile.css';
+import Application from "../../Application";
 
 class UserPage extends Component {
+  constructor() {
+    super();
+    this.fetchProfile();
+  }
   state = {
-    loading: false,
-    loadingMinor: false,
-    error: false,
-    redirect: false,
-    name: 'Jack'
+    name: null
   };
 
   render = () => {
-    const name = this.state.name;
     return (
       <div className="Profile">
         <h1 className="h1-user">User page</h1>
-        <h1 className="name">{name}</h1>
+        <h1 className="name">{this.state.name}</h1>
       </div>
     )
+  };
+
+  fetchProfile = () => {
+    Application.userRepository.getProfileData()
+      .subscribe(profile => {
+        this.setState({name: profile.name});
+      })
   }
 }
 
