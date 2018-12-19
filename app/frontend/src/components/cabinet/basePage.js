05a326ad2f641be7b0d332966fadb356fc3e90a1
evgenii <kotlyarov.ev@gmail.com>
2018-12-19 18:06:37 +0300

    Добавил Базовый кабинет для профайла

---
+++ app/frontend/src/components/cabinet/basePage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Navbar from "../navbar/Navbar";
+
+export default class BaseCabinetPage extends Component {
+  renderContent = () => {
+    throw new Error('You have to implement the method doSomething');
+  };
+
+  render = () => {
+    return(
+      <div>
+        <Navbar/>
+        {this.renderContent()}
+      </div>
+    );
+  }
+}
