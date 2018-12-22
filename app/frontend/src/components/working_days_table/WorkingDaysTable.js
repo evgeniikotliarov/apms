5f41b3ee9f2c28d65ac2116af5a5006348d1c4ef
evgenii <kotlyarov.ev@gmail.com>
2018-12-22 23:21:42 +0300

    Добавил файлы таблицы рабочих дней.

---
+++ app/frontend/src/components/working_days_table/WorkingDaysTable.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Application from "../../Application";
+
+export default class  WorkingDaysTable extends Component {
+  constructor() {
+    super();
+    this.fetchTimeSheets();
+  }
+
+  fetchTimeSheets() {
+
+  }
+}
