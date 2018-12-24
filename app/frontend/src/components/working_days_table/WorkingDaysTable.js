0f4bd63e8c9e655410ce8430cbe2f8d433ed92d9
evgenii <kotlyarov.ev@gmail.com>
2018-12-24 18:25:46 +0300

    Сделал таблицу рабочих дней

--- app/frontend/src/components/working_days_table/WorkingDaysTable.js
+++ app/frontend/src/components/working_days_table/WorkingDaysTable.js
@@ -1,13 +1,54 @@
 import React, {Component} from 'react';
+import './WorkingDaysTable.css';
 import Application from "../../Application";
+import DayCage from "./DayCage";
 
-export default class  WorkingDaysTable extends Component {
+
+export default class WorkingDaysTable extends Component {
   constructor() {
     super();
     this.fetchTimeSheets();
   }
 
   fetchTimeSheets() {
-
+    Application.timeSheetsUseCase.getTimeSheetForCurrentDate()
+      .subscribe(timeSheet => {
+        console.log(timeSheet);
+        this.setState({tableSheets: timeSheet.sheetsDay});
+      });
   }
+
+  state = {
+    year: '',
+    month: '',
+    allMonth: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август",
+      "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+    tableSheets: []
+  };
+
+  render = () => {
+    const currentDay = new Date();
+    const year = currentDay.getFullYear();
+    const month = currentDay.getMonth();
+    const convertMonth = this.state.allMonth[month];
+
+
+    return (
+      <div>
+        <div className="date">{convertMonth}-{year}</div>
+        <div className="table">
+          {this.renderCages()}
+        </div>
+      </div>
+    )
+  };
+
+  renderCages() {
+    return this.state.tableSheets.map((day, index ) => {
+      return (
+        <div key={index}>
+          <DayCage day={day} />
+        </div>)
+    })
+  };
 }
