25f1d0c25f8bd46bfe567120e7d3e18ff3651d36
evgenii <kotlyarov.ev@gmail.com>
2018-12-26 08:45:05 +0300

    Переделал навбар

--- app/frontend/src/components/cabinet/navbar/Navbar.js
+++ app/frontend/src/components/cabinet/navbar/Navbar.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Application from "../../../Application";
 import './Navbar.css';
 
-export default function Navbar(props) {
-  return(
-    <div className="Navbar">
-      <ul className="ul">
-        <li className="li"><a href="#">My statistic</a></li>
-        <li className="li"><a href="#">Other</a></li>
-      </ul>
-    </div>
-  )
+class Navbar extends Component {
+  state = {
+    name: ''
+  };
+
+  componentWillMount = () => {
+    Application.userUseCase.getProfile()
+      .subscribe(profile => {
+        this.setState({name: profile.name});
+      });
+  };
+  render = () => {
+    return (
+      <div className="Navbar">
+        <ul className="ul">
+          <li className="li"><a href="#">My statistic</a></li>
+          <li className="li"><a href="#">Other</a></li>
+          <li className="li name"><a href="#">{this.state.name}</a></li>
+          <li className="li name"><a href="#">Logout</a></li>
+        </ul>
+      </div>
+    )
+  }
 }
+
+export default Navbar;
