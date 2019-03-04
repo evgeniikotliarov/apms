212c7e3c1a6418ebfa8a808a24a2399418fa4e7f
evgenii <kotlyarov.ev@gmail.com>
2019-03-04 10:01:07 +0300

    Сделал профайл страницу.

--- app/frontend/src/components/cabinet/profile/ProfileInfo.js
+++ app/frontend/src/components/cabinet/profile/ProfileInfo.js
@@ -9,10 +9,32 @@ class ProfileInfo extends BaseModal {
   render = () => {
     return this.props.profile !== undefined ? (
       <div className="profile-info">
-        <h2>Hello</h2>
-        <div>
-          <p>Имя пользователя:</p>
-          <p>{this.props.profile.name}</p>
+        <div className="user-data">
+          <p>Имя пользователя:<span>{this.props.profile.name}</span></p>
+        </div>
+        <div className="user-data">
+          <p>Email пользователя:<span>{this.props.profile.email}</span></p>
+        </div>
+        <div className="user-data">
+          <p>Дата поступления:<span>{this.props.profile.employment_date}</span></p>
+        </div>
+        <div className="user-data">
+          <p>Дата активации:<span>{this.props.profile.acceptance_date}</span></p>
+        </div>
+        <div className="user-data">
+          <p>Активирован:<span>{this.props.profile.activated === true ? "Да" : "Нет"}</span></p>
+        </div>
+        <div className="user-data">
+          <p>Админ:<span>{this.props.profile.is_admin === true ? "Да" : "Нет"}</span></p>
+        </div>
+        <div className="user-data">
+          <p>Тариф отпуска:<span>{this.props.profile.rate}</span></p>
+        </div>
+        <div className="user-data">
+          <p>Количество отпускных дней:<span>{this.props.profile.vacation}</span></p>
+        </div>
+        <div className="user-data">
+          <button className="info-edit">Редактировать</button>
         </div>
       </div>
     ) : null
